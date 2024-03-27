b1e1cc390fefce9fef9cf208bb582163a8f8138e
Bhavya Nanavati <pratiknanavati1997@gmail.com>
2024-03-27 22:45:03 +0530

    new file:   Heart_Attack.xlsx 	new file:   Heart_Attack_Prediction.pkl 	modified:   main.py 	modified:   templates/index.html 	modified:   templates/no.html

--- main.py
+++ main.py
@@ -2,36 +2,41 @@ from flask import Flask,render_template,request
 import joblib  
 import pandas as pd  
 import numpy as np 
-# model = joblib.load('Heart_Disease_Prediction.pkl')
+model = joblib.load('Heart_Attack_Prediction.pkl')
 app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def index():
     return render_template('index.html')
 
-# @app.route("/predict", methods=["POST"])  
-# def predict():  
-#    age = request.form["age"]  
-#    sex = request.form["sex"]  
-#    trestbps = request.form["trestbps"]  
-#    chol = request.form["chol"]  
-#    oldpeak = request.form["oldpeak"]  
-#    thalach = request.form["thalach"]  
-#    fbs = request.form["fbs"]  
-#    exang = request.form["exang"]  
-#    slope = request.form["slope"]  
-#    cp = request.form["cp"]  
-#    thal = request.form["thal"]  
-#    ca = request.form["ca"]  
-#    restecg = request.form["restecg"]  
-#    arr = np.array([[age, sex, cp, trestbps,  
-#             chol, fbs, restecg, thalach,  
-#             exang, oldpeak, slope, ca,  
-#             thal]])  
-#    pred = model.predict(arr)  
-#    if pred == 0:  
-#      return render_template('no.html')  
-#    else:  
-#      return render_template('yes.html') 
+@app.route("/predict", methods=["POST"])  
+def predict():  
+   age = float(request.form["age"])  
+   sex = float(request.form["sex"] ) 
+   cp = float(request.form["chestpain"])
+   trestbps = float(request.form["resting_blood_pressure"])  
+   chol =float( request.form["cholesterol"])  
+   fbs = float(request.form["fasting_blood_sugar"])
+   restecg = float(request.form["resting_electrocardiographic"])    
+
+   thalach = float(request.form["max_heart_rate"])
+
+   exang = float(request.form["exercise_induced_angina"] ) 
+   oldpeak = float(request.form["oldpeak"]) 
+   slope = float(request.form["slope"])   
+   ca = float(request.form["num_major_vessels"])    
+   thal = float(request.form["thalassemia"])  
+     
+     
+   arr = np.array([[age, sex, cp, trestbps,  
+            chol, fbs, restecg, thalach,  
+            exang, oldpeak, slope, ca,  
+            thal]])  
+   pred = model.predict(arr)  
+   print(pred)
+   if pred == 0:  
+     return render_template('no.html')  
+   else:  
+     return render_template('yes.html') 
 if  __name__=='__main__':
     app.run(debug=True)
