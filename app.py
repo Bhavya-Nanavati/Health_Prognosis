7f28394abbf4ac2c3cbe807fc2cdbe0aedb6238b
Bhavya Nanavati <pratiknanavati1997@gmail.com>
2024-04-01 19:13:56 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def predict():
      return render_template('no.html')  
    else:  
      return render_template('yes.html') 
-if  __name__=='__main__':
+if  __name__=='__app__':
     app.run(debug=True)
