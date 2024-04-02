b6616d9b65c6c6fd70d5d6a72de1a9e1364fb26b
Bhavya Nanavati <pratiknanavati1997@gmail.com>
2024-04-02 20:11:56 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 import pickle
-import joblib  
+ 
 import numpy as np 
 with open("Heart_Attack_Prediction.pkl", "rb") as f:
   model = pickle.load(f)
